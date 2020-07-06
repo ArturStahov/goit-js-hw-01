da0ebd3c1787bd86d2d70bd670f55416651d77b7
ArturStahov <vtemy.at.ua@gmail.com>
2020-07-06 23:49:28 +0300

    fix task 4

--- task-4.js
+++ task-4.js
@@ -4,17 +4,23 @@ let credits = 23580;
 const pricePerDroid = 3000;
 let totalPrice;
 let quantityPerDroid = prompt("сколько дроидов вы хотите купить");
-quantityPerDroid = Number(quantityPerDroid);
 
-if (quantityPerDroid === 0) {
+if (quantityPerDroid === null) {
   console.log("Отменено пользователем!");
 } else {
-  totalPrice = quantityPerDroid * pricePerDroid;
-  if (totalPrice > credits) {
-    console.log("Недостаточно средств на счету!");
+  let quantityPerDroidConvert = Number(quantityPerDroid);
+
+  if (quantityPerDroidConvert !== 0) {
+    totalPrice = quantityPerDroidConvert * pricePerDroid;
+
+    if (totalPrice > credits) {
+      console.log("Недостаточно средств на счету!");
+    } else {
+      credits -= totalPrice;
+      let message = `Вы купили ${quantityPerDroidConvert} дроидов, на счету осталось ${credits} кредитов.`;
+      console.log(message);
+    }
   } else {
-    credits -= totalPrice;
-    let message = `Вы купили ${quantityPerDroid} дроидов, на счету осталось ${credits} кредитов.`;
-    console.log(message);
+    console.log("Вы не можете купить 0 дроидов повторите попитку снова!");
   }
 }
