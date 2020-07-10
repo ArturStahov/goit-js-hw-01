bbbb42203a366ecd143dc87a393c2000a6e6dd91
ArturStahov <vtemy.at.ua@gmail.com>
2020-07-10 23:09:29 +0300

    fix 5 task v complete

--- js/task-4New.js
+++ js/task-4New.js
@@ -1,15 +1,15 @@
 "use strict";
 
-let textInputRef = document.querySelector(".user-input-form__input");
-let buttonRef = document.querySelector(".user-input-form__submit-button");
-let resultOutputBlock = document.querySelector(".result");
+const textInputRef = document.querySelector(".user-input-form__input");
+const buttonRef = document.querySelector(".user-input-form__submit-button");
+const resultOutputBlock = document.querySelector(".result");
 
 let credits = 23580;
 const pricePerDroid = 3000;
 let totalPrice;
 let message = "Вы не можете купить 0 дроидов повторите попитку снова!";
-let userBalanceBlock = document.querySelector(".user-balance-output");
-let priceBlock = document.querySelector(".price-output");
+const userBalanceBlock = document.querySelector(".user-balance-output");
+const priceBlock = document.querySelector(".price-output");
 
 userBalanceBlock.innerHTML = `${credits}`;
 priceBlock.innerHTML = `${pricePerDroid}`;
@@ -18,14 +18,14 @@ buttonRef.addEventListener("click", function() {
   event.preventDefault();
   let quantityPerDroid = textInputRef.value;
   let quantityPerDroidConvert = Number(quantityPerDroid);
-  let IsNaNinput = Number.isNaN(quantityPerDroidConvert);
+  const isNaNinput = Number.isNaN(quantityPerDroidConvert);
 
   if (quantityPerDroidConvert !== 0 && quantityPerDroid !== null) {
     totalPrice = quantityPerDroidConvert * pricePerDroid;
 
     if (totalPrice > credits) {
       message = "Недостаточно средств на счету!";
-    } else if (IsNaNinput === true) {
+    } else if (isNaNinput === true) {
       message = "Вы ввели некоректное значение!";
     } else {
       credits -= totalPrice;
